45266b32b48c819b633c2a0fc05cbcd79282fc1a
dnorton <daniel.norton@leanlawlabs.com>
2024-07-24 14:05:08 -0400

    updates

--- helloworld/hello.go
+++ helloworld/hello.go
@@ -2,29 +2,34 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
+const (
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
 
-const spanish = "Spanish"
-const french = "French"
+	spanish = "Spanish"
+	french  = "French"
+)
 
 func Hello(name string, language string) string {
-
-	prefix := englishHelloPrefix
-
-	switch language {
-	case spanish:
-		prefix = spanishHelloPrefix
-	case french:
-		prefix = frenchHelloPrefix
-	}
 
 	if name == "" {
 		name = "world"
 
 	}
-	return prefix + name
+	return languagePrefix(language) + name
+}
+
+func languagePrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
